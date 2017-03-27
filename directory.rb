a1596b8d9ff3284b746b0ca41dc2e80d3245c848
Adam Snow <adamjohnsnow@icloud.com>
2017-03-27 12:35:17 +0100

    Now asks for user input

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+=begin
 #put the students in an array
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
@@ -12,6 +13,8 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
+
 def print_header
 puts "The students of Villains Academy"
 puts "-------------"
@@ -27,6 +30,26 @@ def print_footer(names)
 puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat
+    while !name.empty? do
+    # add the hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get next name from the user
+    name = gets.chomp
+    end
+  # return the array
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
