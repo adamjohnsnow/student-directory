336ac946c535a8a696c31ab345b8648d6ac45129
Adam Snow <adamjohnsnow@icloud.com>
2017-03-29 17:51:38 +0100

    something isnt right

--- directory_from_file.rb
+++ directory_from_file.rb
@@ -10,7 +10,7 @@ end
 
 def print_list_of_students
   $cohorts.each do |x|
-if (@students.count{|c| c[:cohort] == x}) >0
+    if (@students.count{|c| c[:cohort] == x}) >0
     puts "#{x.to_s.capitalize} cohort\:"
     @students.each do |student,ind|
       if student[:cohort] == x
@@ -24,23 +24,19 @@ end
 def print_footer(names)
   if names.count > 1
     puts
-puts "Overall, we have #{names.count} great students"
-puts
-else
-  puts
-  puts "We have a great student"
-  puts
-end
+    puts "Overall, we have #{names.count} great students"
+    puts
+  else
+    puts
+    puts "We have a great student"
+    puts
+  end
 end
 
 def input_students
-
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-
   coh = -1
-  # get the first name
   name = gets.strip
 
   # while the name is not empty, repeat
@@ -71,8 +67,9 @@ def input_students
   # return the array
   @students
 end
+
 def save_students
-  file = File.open("students.csv", "w")
+  file = File.open(filename, "w")
   # iterate over the array
   @students.each do |student|
     student_data = [student[:name], student[:cohort], student[:hobby], student[:pob], student[:height]]
@@ -96,27 +93,28 @@ end
 def interactive_menu
 
   loop do
+    selection = ""
       print_menu
     # read the input and save it into a variable
     selection = gets.chomp
     # do what the user has asked
     case selection
-    when "1"
-      @students = input_students
-    when "2"
-      if @students.length > 0
-      show_students
+      when "1"
+        @students = input_students
+      when "2"
+        if @students.length > 0
+            show_students
+        else
+            puts "Sorry, we don\'t currently have any students..."
+        end
+      when "3"
+        save_students
+      when "4"
+        load_students
+      when "9"
+        exit
       else
-        puts "Sorry, we don\'t currently have any students..."
-      end
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
+        puts "I don't know what you mean, try again"
     end
   end
 end
@@ -127,13 +125,27 @@ def show_students
   print_footer(@students)
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-  name, cohort, hobby, pob, height = line.chomp.split(',')
+    name, cohort, hobby, pob, height = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym, hobby: hobby, pob: pob, height: height}
   end
   file.close
 end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+#try_load_students
 
 interactive_menu
